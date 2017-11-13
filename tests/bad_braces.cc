20494ea1ed02ef7204d49def638a931c56db9f4d
Francis Y. Yan <fyy@cs.stanford.edu>
2017-11-13 10:23:21 -0800

    Add tests for braces

---
+++ tests/bad_braces.cc
@@ -0,0 +1,28 @@
+class Fun {
+};
+
+int main(int argc, char * argv[]) {
+  while (true)
+  {
+    break;
+  }
+
+  for (int i = 0; i < 1; ++i)
+  {
+    switch (i)
+    {
+      default:
+        break;
+    }
+
+    if (i == 0)
+    {
+      break;
+    }
+  }
+
+  do
+  {
+    break;
+  } while (true);
+}
---
+++ tests/good_braces.cc
@@ -0,0 +1,25 @@
+class Fun
+{
+};
+
+int main(int argc, char * argv[])
+{
+  while (true) {
+    break;
+  }
+
+  for (int i = 0; i < 1; ++i) {
+    switch (i) {
+      default:
+        break;
+    }
+
+    if (i == 0) {
+      break;
+    }
+  }
+
+  do {
+    break;
+  } while (true);
+}
