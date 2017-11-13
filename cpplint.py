5cf73508b3ef4f031c7c1882e8f35bac7403adbf
Francis Y. Yan <fyy@cs.stanford.edu>
2017-11-13 00:20:12 -0800

    Allow using multi-line comments

--- cpplint.py
+++ cpplint.py
@@ -4253,17 +4253,6 @@ def CheckAltTokens(filename, clean_lines, linenum, error):
   if Match(r'^\s*#', line):
     return
 
-  # Last ditch effort to avoid multi-line comments.  This will not help
-  # if the comment started before the current line or ended after the
-  # current line, but it catches most of the false positives.  At least,
-  # it provides a way to workaround this warning for people who use
-  # multi-line comments in preprocessor macros.
-  #
-  # TODO(unknown): remove this once cpplint has better support for
-  # multi-line comments.
-  if line.find('/*') >= 0 or line.find('*/') >= 0:
-    return
-
   for match in _ALT_TOKEN_REPLACEMENT_PATTERN.finditer(line):
     error(filename, linenum, 'readability/alt_tokens', 2,
           'Use operator %s instead of %s' % (
